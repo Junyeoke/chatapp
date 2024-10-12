f1bc11d45f9bf44b904f616eabb3aeae00107b65
Junyeoke <126323071+Junyeoke@users.noreply.github.com>
2024-10-12 22:11:47 +0900

    주석추가
    주석추가

--- src/main/java/com/example/chatproject/config/WebConfig.java
+++ src/main/java/com/example/chatproject/config/WebConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * WebConfig 클래스는 Spring MVC의 CORS(Cross-Origin Resource Sharing) 설정을 구성합니다.
+ * CORS는 다른 출처의 웹 페이지가 현재 출처의 리소스에 접근할 수 있도록 허용하는 메커니즘입니다.
+ * 이 설정을 통해 특정 출처에서 오는 요청을 허용하고, 허용할 HTTP 메서드 및 자격 증명 사용 여부를 지정합니다.
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
--- src/main/java/com/example/chatproject/config/WebSocketConfig.java
+++ src/main/java/com/example/chatproject/config/WebSocketConfig.java
@@ -8,6 +8,11 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
+/**
+ * WebSocketConfig 클래스는 Spring WebSocket 메시지 브로커를 설정하는 클래스이다.
+ * 이 클래스에서는 STOMP 프로토콜을 사용하여 웹소켓 엔드포인트를 등록하고,
+ * 메시지 브로커를 구성하여 클라이언트와 서버 간의 실시간 메시지 전송을 가능하게 한다.
+ */
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -15,16 +20,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // STOMP 엔드포인트 등록
-        registry.addEndpoint("/ws") // 웹소켓 엔드포인트 설정
-                .setAllowedOriginPatterns("http://localhost:8080") // 허용할 출처 설정
-                .withSockJS() // SockJS 사용 설정
-                .setInterceptors(new CustomHttpSessionInterceptor()); // 커스텀 인터셉터 사용
+        registry.addEndpoint("/ws") // 웹소켓 엔드포인트를 "/ws"로 설정
+                .setAllowedOriginPatterns("http://localhost:8080") // 허용할 출처를 설정
+                .withSockJS() // SockJS를 사용하여 브라우저 호환성 제공
+                .setInterceptors(new CustomHttpSessionInterceptor()); // 커스텀 HTTP 세션 인터셉터 사용
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         // 메시지 브로커 설정
         config.enableSimpleBroker("/topic"); // 메시지 브로커가 "/topic" 경로를 구독하도록 설정
-        config.setApplicationDestinationPrefixes("/app"); // 애플리케이션 목적지 접두사 설정
+        config.setApplicationDestinationPrefixes("/app"); // 애플리케이션 목적지의 접두사를 "/app"으로 설정
     }
 }
